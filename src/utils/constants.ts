5740c9f60e0b452c88d6859f25a7734060362f0a
Naptie <naptie@qq.com>
2025-11-01 00:02:10 +0800

    Revert "fix: add BASE_PREFIX support for dynamic base URLs"
    This reverts commit bfbab4f29d3763f2d1b3af17053435ee96f9befd.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,8 @@
 export const APP_NAME = 'scut-notipay';
 export const GITHUB_LINK = 'https://github.com/Naptie/scut-notipay';
 
-const BASE_PREFIX = process.env.BASE_PREFIX || '';
-export const CARD_BASE = `${BASE_PREFIX}https://ecardwxnew.scut.edu.cn`;
-export const DFYC_BASE = `${BASE_PREFIX}https://dfyc.utc.scut.edu.cn`;
+export const CARD_BASE = 'https://ecardwxnew.scut.edu.cn';
+export const DFYC_BASE = 'https://dfyc.utc.scut.edu.cn';
 
 export const CAMPUSES = ['GZIC', 'DXC'] as const;
 
